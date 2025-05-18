8b6aa07f8b54ea197f55d11082910ec16c79b71b
ckejoM <156849470+ckejoM@users.noreply.github.com>
2025-05-18 15:01:21 +0200

    Remove UserId from Category

--- src/backend/TaskManagementApp/Domain/Common/BaseEntity.cs
+++ src/backend/TaskManagementApp/Domain/Common/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Domain.Common
         public DateTime? ModifiedOn { get; set; }
         public Guid? ModifiedBy { get; set; }
         public bool IsDeleted { get; set; }
+        [Timestamp]
         public byte[] RowVersion { get; set; }
     }
 }
