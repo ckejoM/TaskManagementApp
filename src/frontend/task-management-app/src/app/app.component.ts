88a5d2b183642b6b74c197dc3f82ae7e913a6b25
ckejofnt <jovanmadzic99@gmail.com>
2025-06-08 17:40:59 +0200

    Add angular material

--- src/frontend/task-management-app/src/app/app.component.ts
+++ src/frontend/task-management-app/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService, LoginRequest } from './shared/apiClient';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers:[AuthService]
