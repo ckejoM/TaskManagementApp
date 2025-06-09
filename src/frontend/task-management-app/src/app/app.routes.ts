6050661ed63b9463ea00f93cd7dc11ff5e765fcd
ckejofnt <jovanmadzic99@gmail.com>
2025-06-09 14:05:49 +0200

    Add main application components (task, project, category) Add routes

--- src/frontend/task-management-app/src/app/app.routes.ts
+++ src/frontend/task-management-app/src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { CategoriesComponent } from './category/categories/categories.component';
+import { TasksComponent } from './task/tasks/tasks.component';
+import { ProjectsComponent } from './project/projects/projects.component';
 
 export const routes: Routes = [
+    {
+        path: 'categories',
+        component: CategoriesComponent
+    },
+    {
+        path: 'tasks',
+        component: TasksComponent
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent
+    },
     {
         path: 'login',
         component: LoginComponent
